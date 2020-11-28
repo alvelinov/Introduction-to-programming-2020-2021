afda25cbcafd6555932e736e58a8a726cb2bbe4e
alvelinov <alvelinov7@gmail.com>
2020-11-28 20:10:22 +0200

    Create problem11.cpp

---
+++ Additional class 2020.11.28/problem11.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+  Въвежда се естествено число от клавиатурата. След това се въвеждат номерата две различни позиции на битове в двоичното му
+  представяне. Програмата трябва да размени битовете на тези две позиции (приемаме, че са подадени валидно). Изведете промененото число.
+  Пример: 10 1 5
+  Изход: 40
+*/
+
+int main()
+{
+    unsigned num;
+    cin >> num;
+
+    unsigned pos1, pos2;
+    cin >> pos1 >> pos2;
+
+    unsigned mask1 = (1 << pos1);
+    unsigned mask2 = (1 << pos2);
+
+    unsigned bit1, bit2;
+
+    bit1 = !!(mask1 & num);
+    bit2 = !!(mask2 & num);
+
+    if (bit1 == bit2)
+        cout << num;
+    else {
+        if (bit1 == 1){
+            num = num | (1 << pos2);
+        } else {
+            num = num & ~(1 << pos2);
+        }
+
+        if (bit2 == 1){
+            num = num | (1 << pos1);
+        } else {
+            num = num & ~(1 << pos1);
+        }
+
+        cout << num;
+    }
+
+    // Алтернативно решение на един ред
+    // ( !!(num & (1 << pos1)) != !!(num & (1 << pos2)) ) ? cout << ( num ^ ((1 << pos1) | (1 << pos2)) ) << endl : cout << num << endl;
+
+    return 0;
+}
